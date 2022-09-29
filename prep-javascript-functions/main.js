e72548bc40cf8f85fdfc673e4e9401b4058d49da
Daniel Virrueta <danielvirrueta@yahoo.com>
2022-09-29 18:47:39 +0000

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,54 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var addTwoNumbersResult = addTwoNumbers(3, 9);
+console.log('Add two numbers result', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var hoursToMinutesResult = convertHoursToMinutes(3);
+console.log('Convert hours to minutes result', hoursToMinutesResult);
+
+function getGreeting(name) {
+  return name;
+}
+var greeting = getGreeting('World');
+console.log('Hello', greeting);
+
+function addAndMultiplyBy5(x, y) {
+  return x + y * 5;
+}
+var addAndMultiplyBy5Result = addAndMultiplyBy5(10, 3);
+console.log('Add and multiply by 5 result', addAndMultiplyBy5Result);
+
+function multiplyAndDivideBy5(x, y) {
+  return x * y / 5;
+}
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(25, 5);
+console.log('Multiply and divide by 5 result', multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(x, y) {
+  return x - y;
+}
+var subtractTwoNumbersResult = subtractTwoNumbers(10, 3);
+console.log('Subtract two numbers result', subtractTwoNumbersResult);
+
+function getCircleCircumference(radius) {
+  return 2 * 3.14 * radius;
+}
+var getCircleCircumferenceResult = getCircleCircumference(9);
+console.log('Get circle curcumference result', getCircleCircumferenceResult);
+
+function getFullName(firstName, lastName) {
+  return firstName + lastName;
+}
+var getFullNameOutput = getFullName('Daniel ', 'Virrueta');
+console.log('My full name:', getFullNameOutput);
+
+function cube(number) {
+  return number * number * number;
+}
+var cubeResult = cube(8);
+console.log('The result of a number being cubed is:', cubeResult);
